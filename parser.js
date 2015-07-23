6b90401e16234c4a2ac6fdeae0d15270ab8ad3b1
Philip Muškovac <yofel@gmx.net>
2015-07-23 20:33:47 +0200

    Put the parsing code into a function
    We'll neeed to loop over all bugs evenutally

--- parser.js
+++ parser.js
@@ -2,58 +2,64 @@
     var fs = require('fs');
     var $ = require('jquery')(require("jsdom").jsdom().parentWindow);
 
-    var bug = {};
-    bug.id = 1;
-
-    var page = fs.readFileSync('/home/yofel/dump/tanglu/cache-dl/tracker.tanglu.org/T' + bug.id + '/index.html', {'encoding': 'utf8'});
-
-    bug.title = $(page).find('.phui-header-view').contents().eq(1).text();
-    bug.status = $(page).find('.phui-header-view').children('.phui-header-subheader').contents().eq(0).text();
-    bug.visibility = $(page).find('.phui-header-view').children('.phui-header-subheader').contents().eq(1).text();
-    bug.assigned = $(page).find("dt:contains('Assigned To')").next().text();
-    bug.priority = $(page).find("dt:contains('Priority')").next().text();
-    bug.author = $(page).find("dt:contains('Author')").next().text();
-    bug.subscribers = $(page).find("dt:contains('Subscribers')").next().text(); // FIXME?
-    bug.projects = $(page).find("dt:contains('Projects')").next().text(); // FIXME?
-    bug.extern = $(page).find("dt:contains('External Issue')").next().text();
-
-    bug.description = {};
-    var _description = $(page).find("div:contains('Description'):last").next();
-    var _desc_header = _description.find('p').eq(0).text();
-
-    // The name is at the front
-    bug.description.author = _desc_header.substring(0, _desc_header.indexOf('wrote') - 1);
-
-    // Take the date and strip the trailing colon
-    // TODO: Timezone?
-    bug.description.date = _desc_header.substring(_desc_header.indexOf('on') + 3, _desc_header.length - 1);
-
-    // bug description
-    bug.description.content = _description.find('p').eq(1).text();
-
-    var _timeline_items = $(page).find('.phui-timeline-view').find('.phui-timeline-shell');
-
-    bug.timeline = [];
-    _timeline_items.each(function (_item) {
-
-        var comment = '';
-        $(this).find('.phui-timeline-title').each(function (_action) {
-
-            // put all actions from the same transaction into one comment
-            comment += $(this).text() + '\n';
-        });
-
-        var _comment = $(this).find('.phui-timeline-core-content').find('p').text();
-
-        // append the actual comment text if there is one
-        if (_comment.length > 0) {
-            comment += _comment;
-        }
-
-        bug.timeline.push(comment);
-    });
-
 
     console.log("\nBUG JSON:");
-    console.log(bug);
+    console.log(parsePage(1));
+
+
+
+    function parsePage(id) {
+        var bug = {};
+        bug.id = id;
+
+        var page = fs.readFileSync('/home/yofel/dump/tanglu/cache-dl/tracker.tanglu.org/T' + bug.id + '/index.html', {'encoding': 'utf8'});
+
+        bug.title = $(page).find('.phui-header-view').contents().eq(1).text();
+        bug.status = $(page).find('.phui-header-view').children('.phui-header-subheader').contents().eq(0).text();
+        bug.visibility = $(page).find('.phui-header-view').children('.phui-header-subheader').contents().eq(1).text();
+        bug.assigned = $(page).find("dt:contains('Assigned To')").next().text();
+        bug.priority = $(page).find("dt:contains('Priority')").next().text();
+        bug.author = $(page).find("dt:contains('Author')").next().text();
+        bug.subscribers = $(page).find("dt:contains('Subscribers')").next().text(); // FIXME?
+        bug.projects = $(page).find("dt:contains('Projects')").next().text(); // FIXME?
+        bug.extern = $(page).find("dt:contains('External Issue')").next().text();
+
+        bug.description = {};
+        var _description = $(page).find("div:contains('Description'):last").next();
+        var _desc_header = _description.find('p').eq(0).text();
+
+        // The name is at the front
+        bug.description.author = _desc_header.substring(0, _desc_header.indexOf('wrote') - 1);
+
+        // Take the date and strip the trailing colon
+        // TODO: Timezone?
+        bug.description.date = _desc_header.substring(_desc_header.indexOf('on') + 3, _desc_header.length - 1);
+
+        // bug description
+        bug.description.content = _description.find('p').eq(1).text();
+
+        var _timeline_items = $(page).find('.phui-timeline-view').find('.phui-timeline-shell');
+
+        bug.timeline = [];
+        _timeline_items.each(function (_item) {
+
+            var comment = '';
+            $(this).find('.phui-timeline-title').each(function (_action) {
+
+                // put all actions from the same transaction into one comment
+                comment += $(this).text() + '\n';
+            });
+
+            var _comment = $(this).find('.phui-timeline-core-content').find('p').text();
+
+            // append the actual comment text if there is one
+            if (_comment.length > 0) {
+                comment += _comment;
+            }
+
+            bug.timeline.push(comment);
+        });
+
+        return bug;
+    }
 }) ();
